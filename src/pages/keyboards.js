833474da1a48a615a357dbea28147ab3b90cde10
scarface96361 <joeyrosenbaum@gmail.com>
2022-10-20 18:01:37 -0400

    Added new keyboard

--- src/pages/keyboards.js
+++ src/pages/keyboards.js
@@ -8,7 +8,8 @@ const IMAGES =[
 	{id: 3,src: "keyboard003.jpg", youtube: "https://youtu.be/WwtxdDv7cVU", description: 'This is the Second comission keyboard I built. This was for my little brother and used a gmmk2 65% alongisde lubed glorious pandas and gsv2 stabilizers.' },
 	{id: 4,src: "keyboard004.jpg", youtube: "https://youtu.be/5QfURUpBXI4", description: "This was the first silent keyboard I built. it was built using a gmmk2 95%, boba U4T's and the glorious ice keycaps." },
 	{id: 5,src: "keyboard005.jpg", youtube: "https://youtu.be/zCaxgVVu4zs", description: "Second silent keyboard I built using a gmmk pro as a base. PBT octopus keycaps and boba U4 silent linear switches." },
-	{id: 6,src: "keyboard006.jpg", youtube: "https://youtu.be/bPEy-d5sVGw", description: "Description pending"},
+	{id: 6,src: "keyboard006.jpg", youtube: "https://youtu.be/bPEy-d5sVGw", description: "Description pending" },
+	{id: 7,src: "keyboard007.jpg", youtube: "https://youtu.be/2uZ3H1AKk-8", description: "A GMMK2 with lubed kailh pro purple switches, gpbt celestial fire keycaps and relubed stabilizers."},
 ]
 //const text = "hello world";
 
